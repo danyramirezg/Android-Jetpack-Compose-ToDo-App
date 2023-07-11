4d4424ea9d98a68fcc10d1bece79fc8fde4fa391
danyramirezg <danyramirezg@gmail.com>
2023-07-11 14:40:07 -0500

    Create the screen and view model of the TO-DO App

---
+++ app/src/main/java/com/example/jetpackcomposetodoapp/ui/screens/TasksScreen.kt
@@ -0,0 +1,145 @@
+package com.example.jetpackcomposetodoapp.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.example.jetpackcomposetodoapp.ui.TasksViewModel
+import com.example.jetpackcomposetodoapp.ui.model.TaskModel
+
+@Composable
+fun TasksScreen(tasksViewModel: TasksViewModel) {
+
+    val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(false)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(
+            showDialog,
+            onDismiss = { tasksViewModel.onDialogClose() },
+            onTaskAdded = { tasksViewModel.onTasksCreated(it) })
+        FabDialog(modifier = Modifier
+            .align(Alignment.BottomEnd)
+            .padding(24.dp), tasksViewModel)
+        TasksList(tasksViewModel)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
+    var myTask by remember {
+        mutableStateOf("")
+    }
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Add the task",
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(value = myTask, onValueChange = { myTask = it })
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = {
+                    // Add task
+                    onTaskAdded(myTask)
+                    myTask = ""
+                }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Add task")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TaskModel> = tasksViewModel.task
+    LazyColumn {
+        items(myTasks, key = { it.id }) { task ->
+            ItemTask(taskModel = task , tasksViewModel = tasksViewModel )
+        }
+    }
+}
+
+@Composable
+fun ItemTask(taskModel: TaskModel, tasksViewModel: TasksViewModel) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .pointerInput(Unit){
+                detectTapGestures( onLongPress = {
+                    tasksViewModel.onItemRemove(taskModel)
+                })
+            }
+        )
+    {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task, modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f)
+            )
+            Checkbox(checked = taskModel.selected,
+                onCheckedChange = { tasksViewModel.onCheckBoxSelected(taskModel) })
+        }
+    }
+}
+
+@Composable
+fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
+    FloatingActionButton(onClick = {
+        // Show Dialog
+        tasksViewModel.onShowDialogClick()
+    }, modifier = modifier) {
+        Icon(Icons.Filled.Add, contentDescription = "")
+    }
+}
+
+@Composable
+@Preview
+fun TasksScreenPreview() {
+    val tasksViewModel = TasksViewModel()
+    TasksScreen(tasksViewModel)
+}
